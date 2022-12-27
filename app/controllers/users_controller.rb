dc6b1d1ee77d2f8d5927e59ebf7aafdc0ace6d5c
renren121314 <renren199612@gmail.com>
2022-12-27 11:11:45 +0000

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,16 @@ class UsersController < ApplicationController
 
   def index
     @book = Book.new
+    @users = User.all
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    end
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -22,13 +26,13 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
-  
+
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
 
 
